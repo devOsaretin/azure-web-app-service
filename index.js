c77ee403fe46185bde81139246ada9448900623f
Osaretin Igbinobaro <mail@osaretin.dev>
2024-04-06 20:40:30 +0100

    add html response

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http')
 const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type":"text/plain"})
-    response.end("Hello World")
+    response.writeHead(200, {"Content-Type":"text/html"})
+    response.end("<html><body><h1>Hello World</h1></body></html>")
 }).listen(port);
 
 console.log(`Server is running at port ${port}`)
